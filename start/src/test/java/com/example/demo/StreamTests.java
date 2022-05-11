889bdd6a27c6bfd1c54ae828f42577a7b5ba123d
where.liu <where.liu@huolala.cn>
2022-05-11 13:38:47 +0800

    feat: save

--- start/src/test/java/com/example/demo/StreamTests.java
+++ start/src/test/java/com/example/demo/StreamTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,14 +38,30 @@ public class StreamTests {
         User two = User.builder().username("b").password("b-p").build();
         List<User> userList = Arrays.asList(one, two);
 
-        List<User> modifiedUserList = userList.stream().map(user -> {
-            user.setUsername("modified");
-            return user;
-        }).collect(Collectors.toList());
+        List<User> modifiedUserList = userList.stream()
+                .map(user -> User.builder().username(user.getUsername()).build())
+                .collect(Collectors.toList());
 
         log.info("{}", userList);
         log.info("{}", modifiedUserList);
     }
+
+    @Test
+    public void flatMap() {
+        User one = User.builder().username("a").password("a-p").build();
+        User two = User.builder().username("b").password("b-p").build();
+        List<User> oneUserList = List.of(one);
+        List<User> twoUserList = List.of(two);
+
+        List<List<User>> allUserList = List.of(oneUserList, twoUserList);
+
+        List<User> userList = allUserList.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        log.info("{}", userList);
+    }
+
 
     @Data
     @Builder

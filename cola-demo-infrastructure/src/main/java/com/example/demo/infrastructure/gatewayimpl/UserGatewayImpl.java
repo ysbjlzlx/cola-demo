9b26179d569654d9d5130bd3041e5ac1d8996dbe
where.liu <where.liu@huolala.cn>
2022-05-17 10:27:10 +0800

    feat: save

--- cola-demo-infrastructure/src/main/java/com/example/demo/infrastructure/gatewayimpl/UserGatewayImpl.java
+++ cola-demo-infrastructure/src/main/java/com/example/demo/infrastructure/gatewayimpl/UserGatewayImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.infrastructure.gatewayimpl;
 
 import com.example.demo.domain.user.gateway.UserGateway;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Where.LIU
  * @since 2022/4/11
  */
+@Component
 public class UserGatewayImpl implements UserGateway {
 }

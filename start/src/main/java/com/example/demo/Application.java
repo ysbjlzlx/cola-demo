c39f9ac513ac4bbd1eaaffc4797dd5f2178a9a47
where.liu <where.liu@huolala.cn>
2022-05-17 10:11:56 +0800

    feat: save

--- start/src/main/java/com/example/demo/Application.java
+++ start/src/main/java/com/example/demo/Application.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
